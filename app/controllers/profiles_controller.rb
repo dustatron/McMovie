177c6971cddbdcdda858e49e1a75be2d7921a9f3
Dusty McCord <dustymccord@gmail.com>
2017-10-07 04:15:23 +0000

    added ability to edit user details

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_user, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+
     
     def index
         @user = current_user
@@ -10,11 +12,23 @@ class ProfilesController < ApplicationController
         
     end
     
+    # PATCH/PUT /profile/1
+    # PATCH/PUT /profile/1.json
     def update
+        respond_to do |format|
+         if @user.update(user_params)
+           format.html { redirect_to profiles_path, notice: 'Movie was successfully updated.' }
+           format.json { render :show, status: :ok, location: @user }
+         else
+           format.html { render :edit }
+           format.json { render json: @user.errors, status: :unprocessable_entity }
+         end
+       end
         
     end
     
     def show
+        @user = User.find(params[:id])
         
     end
 
@@ -24,8 +38,7 @@ class ProfilesController < ApplicationController
     end
     
     def edit
-     @user = User.find_by(id: current_user.id)
-        
+      @user = User.find(params[:id])
     end
     
     def destroy
@@ -33,9 +46,13 @@ class ProfilesController < ApplicationController
     
     
     private
-    
-    def set_user
-       @user = User.find_by(id: params[:id])
+      
+      def set_user
+         @user = User.find(params[:id])
+      end
+      
+    def user_params
+      params.require(:user).permit(:name, :email, :tagline, :bio, :photo)
     end
-
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/search' => 'search#index'
   
  #user Profiles
- resources :profiles
+ resources :profiles 
 
  #movie read outs
   resources :movies do

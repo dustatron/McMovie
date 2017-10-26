a9452cc46adfa426dcf6603a384a6a5dd87706b3
Dusty McCord <dustymccord@gmail.com>
2017-10-25 21:45:58 -0700

    worked on error messages for login and sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, :only => [:create]
 
+  rescue_from ActiveRecord::RecordNotUnique, :with => :my_rescue_method
+
   protected
+    def my_rescue_method
+      flash[:alert] = "User name is use"
+      redirect_to new_user_registration_path
+
+    end
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
     end
 end

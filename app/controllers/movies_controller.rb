22fe1c065c185e8ee7052b549ac802fb54498d97
Dusty McCord <dustymccord@gmail.com>
2017-11-01 23:28:56 +0000

    fixed redirection but in movies controller.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,11 +39,12 @@ class MoviesController < ApplicationController
     details = Details.for(params[:movie]['id'])
     grabThe = Grabs.new(details, @user)
     @movie = grabThe.movie
+    
 
 
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.html { redirect_to movie_path(@movie), notice: 'Movie was successfully created.' }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new }

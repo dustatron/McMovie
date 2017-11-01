019b0547b891beaacffce6285d2db5009d92be46
Dusty McCord <dustymccord@gmail.com>
2017-11-01 23:21:20 +0000

    updated user model with a higher limit on the bio.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   acts_as_voter
 
   validates_length_of :tagline, maximum: 25
-  validates_length_of :bio, maximum: 50
+  validates_length_of :bio, maximum: 250
   validates_length_of :name, maximum: 20
   
   # Include default devise modules. Others available are:

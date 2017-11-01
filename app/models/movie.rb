e2e4a0a06dbf275e2467550406f8643209462eac
Dusty McCord <dustymccord@gmail.com>
2017-11-01 23:33:35 +0000

    updated work count limit for movie body.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
     validates_length_of :title, maximum: 50
     validates_length_of :trailer, maximum: 25
     validates_length_of :tagline, maximum: 50
-    validates_length_of :body, maximum: 500
+    validates_length_of :body, maximum: 1000
     acts_as_votable
     has_many :comments, dependent: :destroy
 end
